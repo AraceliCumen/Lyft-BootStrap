225b584f28645af11d0f5bc248596d9887f6fed5
AraceliCumen <aycuevam@gmail.com>
2018-01-02 02:15:35 -0500

    Subiendo vista register y actualizando css

--- js/register.js
+++ js/register.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-  console.log(window.location);
-  setTimeout(function() { 
-    window.location.href = 'signup.html';
-  }, 8000);
-  /* // Seleccionamos los elementos del DOM
+  // Seleccionamos los elementos del DOM
   var $btnSignUp = $('#signup');
   var $btnLogin = $('#login');
-  // Asociamos el evento click
+  //  Asociamos el evento click
   $btnSignUp.on('click', function() {
     alert('hola');
-  }); */
+  });
 });
